1d7b1b18e4717da4c23b03d2159976c525507938
ksandr <zandrewboyle@gmail.com>
2025-04-21 11:58:30 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        tushinskaya: resolve(__dirname, 'tushinskaya.html'),
+        saratovskaya: resolve(__dirname, 'saratovskaya.html'),
+        goncharnaya: resolve(__dirname, 'goncharnaya.html')
+      }
+    }
   },
   server: {
     open: true
