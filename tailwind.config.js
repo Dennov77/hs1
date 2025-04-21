d86f051f1847e470d2d6df7e98e94dda89ce1c36
ksandr <zandrewboyle@gmail.com>
2025-04-21 10:51:02 +0300

    upd

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2E7D32',
+        'primary-light': '#4CAF50',
+        'primary-dark': '#1B5E20',
+        secondary: '#81C784',
+        accent: '#00C853',
+      }
+    }
+  },
+  plugins: [],
+}
